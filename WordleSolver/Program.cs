72759104b577c431682b16719d37d38998ae324e
Hayato Ikeda <95620738+HayatoIkeda19890415@users.noreply.github.com>
2023-03-30 22:07:18 +0900

    create outlines

--- WordleSolver/Program.cs
+++ WordleSolver/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+
+string? input = null;
+if (args.Length != 0)
+{
+    input = args[0];
+}
+
+string word;
+if (input == null)
+{
+    word = suggestWord();
+}
+else
+{
+    word = guessWord(input);
+}
+
+Console.WriteLine(word);
+
+string suggestWord()
+{
+    return string.Empty;
+}
+
+string guessWord(string input)
+{
+    return string.Empty;
+}
