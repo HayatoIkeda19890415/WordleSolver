135f802a051af0930f39efeec8ad0107405cbfd0
Hayato Ikeda <95620738+HayatoIkeda19890415@users.noreply.github.com>
2023-04-08 20:10:12 +0900

    add guess procedure (can't solve all)

--- WordleSolver/Program.cs
+++ WordleSolver/Program.cs
@@ -3,9 +3,21 @@
 
 using System.Text.RegularExpressions;
 
-const string PATH_DICT = @".\Dictionary\";
-Dictionary<char, String[]> dictionaries = new Dictionary<char, String[]>();
-List<char> ng_Chars = new List<char>();
+const string PATH_DICT = @".\Dictionary\words.txt";
+List<char> mustChars = new List<char>();
+var initCharCondition = @"[abcdifghijklmnopqrstuvwxyz]";
+var char1cond = initCharCondition;
+var char2cond = initCharCondition;
+var char3cond = initCharCondition;
+var char4cond = initCharCondition;
+var char5cond = initCharCondition;
+var searchcond = @"^" + char1cond + char2cond + char3cond + char4cond + char5cond + "$";
+var reader = new StreamReader(File.OpenRead(PATH_DICT));
+string[]? lines = reader.ReadToEnd().Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.None);
+if (lines == null)
+{
+    throw new Exception("Coudn't find " + PATH_DICT);
+}
 
 string word = guessWord(string.Empty, string.Empty);
 Console.WriteLine(word);
@@ -28,57 +40,109 @@ string guessWord(string? input, string latestWord)
 
 string pickRandomWord()
 {
-    var rand = new Random();
-    var randInt = rand.Next('a', ('z' + 1));
-    var randChar = (char)randInt;
-    string[]? lines;
-    if (!dictionaries.ContainsKey(randChar))
-    {
-        var reader = new StreamReader(File.OpenRead(PATH_DICT + randChar + ".txt"));
-        lines = reader.ReadToEnd().Split(Environment.NewLine);
-        dictionaries.Add(randChar, lines);
-    }
-    else
-    {
-        lines = dictionaries.GetValueOrDefault(randChar);
-    }
-    if (lines == null)
-    {
-        throw new Exception("Coudn't find " + randChar + ".txt dictionary");
-    }
-    var onlyAlphabet = @"[a-z]{5}";
     var randLineNum = new Random().Next(0, lines.Count());
-    var match = Regex.Match(lines[randLineNum], onlyAlphabet);
+    var match = Regex.Match(lines[randLineNum], searchcond);
     while (!match.Success)
     {
         randLineNum = new Random().Next(0, lines.Count());
-        match = Regex.Match(lines[randLineNum], onlyAlphabet);
+        match = Regex.Match(lines[randLineNum], searchcond);
     }
     return lines[randLineNum];
 }
 
-const char FAILURE = '0';
-const char CHARONLY = '1';
-const char SUCCESS = '2';
 string searchWord(string input, string latestWord)
 {
     char[] resultArray = input.ToCharArray();
     char[] wordArray = latestWord.ToCharArray();
-    foreach (char c in resultArray)
+    searchcond = modSearchCondition(resultArray, wordArray);
+
+    foreach (var item in lines)
     {
+        if (Regex.Match(item, searchcond).Success)
+        {
+            var mustChar_isContained = true;
+            foreach (var mustchar in mustChars)
+            {
+                if (!item.Contains(mustchar))
+                {
+                    mustChar_isContained = false;
+                    break;
+                }
+            }
+            if (mustChar_isContained)
+            {
+                return item;
+            }
+        }
+    }
+    throw new Exception("Coudn't find the word.");
+}
+
+string modSearchCondition(char[] resultArray, char[] wordArray)
+{
+    const char FAILURE = '0';
+    const char CHARONLY = '1';
+    const char SUCCESS = '2';
+
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        var charString = wordArray[i].ToString();
+        char c = resultArray[i];
         if (c == FAILURE)
         {
-            dictionaries.Remove(c);
-            ng_Chars.Add(c);
+            char1cond = char1cond.Replace(charString, "");
+            char2cond = char2cond.Replace(charString, "");
+            char3cond = char3cond.Replace(charString, "");
+            char4cond = char4cond.Replace(charString, "");
+            char5cond = char5cond.Replace(charString, "");
         }
         else if (c == CHARONLY)
         {
-
+            mustChars.Add(wordArray[i]);
+            if (i == 0)
+            {
+                char1cond = char1cond.Replace(charString, "");
+            }
+            else if (i == 1)
+            {
+                char2cond = char2cond.Replace(charString, "");
+            }
+            else if (i == 2)
+            {
+                char3cond = char3cond.Replace(charString, "");
+            }
+            else if (i == 3)
+            {
+                char4cond = char3cond.Replace(charString, "");
+            }
+            else if (i == 4)
+            {
+                char5cond = char3cond.Replace(charString, "");
+            }
         }
         else if (c == SUCCESS)
         {
-
+            if (i == 0)
+            {
+                char1cond = charString;
+            }
+            else if (i == 1)
+            {
+                char2cond = charString;
+            }
+            else if (i == 2)
+            {
+                char3cond = charString;
+            }
+            else if (i == 3)
+            {
+                char4cond = charString;
+            }
+            else if (i == 4)
+            {
+                char5cond = charString;
+            }
         }
     }
-    return pickRandomWord();
+    return @"^" + char1cond + char2cond + char3cond + char4cond + char5cond + "$";
 }
